b911abe7529c45b45e3fa8358cc37a0b60f0ed3d
YiLiuBa <yiliuba168.iot@gmail.com>
2025-07-18 13:36:13 +0700

    chore: Removed t420

--- machines/t420/home/ditatompel/.config/nvim/lua/custom/plugins/init.lua
+++
@@ -1,8 +0,0 @@
--- You can add your own plugins here or in other files in this directory!
--- See the kickstart.nvim README for more information
-return {
-  { 'lambdalisue/suda.vim' },
-  { 'wakatime/vim-wakatime', lazy = false },
-}
-
--- vim: ts=2 sts=2 sw=2 et
--- machines/t420/sync-git.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Sync changed files to repo
-# MUST be executed from current script direcotry
-
-rsync / ~/.local/src/dt/ditatompel/machines/t420/ --include-from ./rsync-include -avh
