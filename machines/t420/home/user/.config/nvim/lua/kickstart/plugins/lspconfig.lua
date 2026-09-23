5c1276097a68fdb21a36b45c07f7634906ab70a9
YiLiuBa <yiliuba168.iot@gmail.com>
2026-09-24 00:09:59 +0700

    t420: syncronize nvim config
    Latest checkpoint from https://github.com/dam9000/kickstart-modular.nvim/commit/96a9cdc841acd1f3d9ca597c58e52eeb77191814

--- machines/t420/home/user/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ machines/t420/home/user/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -138,7 +138,7 @@ local servers = {
   goimports = {},
   templ = { filetypes = { 'templ' } },
   htmx = { filetypes = { 'html' } },
-  ts_ls = {},
+  tsc = {},
   svelte = { filetypes = { 'svelte' } },
 
   stylua = {}, -- Used to format Lua code
@@ -153,7 +153,8 @@ local servers = {
         if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
       end
 
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      local current_settings = client.config.settings --[[@as lspconfig.settings.lua_ls]]
+      client.config.settings.Lua = vim.tbl_deep_extend('force', current_settings.Lua, {
         runtime = {
           version = 'LuaJIT',
           path = { 'lua/?.lua', 'lua/?/init.lua' },
@@ -162,10 +163,7 @@ local servers = {
           checkThirdParty = false,
           -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
           --  See https://github.com/neovim/nvim-lspconfig/issues/3189
-          library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
-            '${3rd}/luv/library',
-            '${3rd}/busted/library',
-          }),
+          library = vim.api.nvim_get_runtime_file('', true),
         },
       })
     end,
